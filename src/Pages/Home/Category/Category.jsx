89377d2f2269ecdd8bad0d2ba400f9c7b30008aa
Mojaer Ahmed <mojaermohi@gmail.com>
2023-05-19 02:16:31 +0600

    category tab is added and header is designed

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 
 const Category = () => {
@@ -12,10 +13,34 @@ const Category = () => {
         fetchToys();
     }, [])
     console.log(toys)
-    return (
-        <div>
 
-        </div>
+    return (
+        <section className="my-10">
+            <h1 className="text-5xl font-bold text-center my-8">Toy Categories: Explore & Choose</h1>
+            <div>
+                <Tabs>
+                    <TabList className='flex justify-evenly flex-wrap mb-3 bg-slate-700'>
+                        <Tab className="mr-2 py-2" >
+                            <h1 className="px-3 border-b-2  border-transparent rounded-b-lg hover:text-red-400 hover:border-red-700 ">DC</h1>
+                        </Tab>
+                        <Tab className="mr-2 py-2">
+                            <h1 className="px-3 border-b-2  border-transparent rounded-b-lg hover:text-red-300 hover:border-red-700 ">Marvel</h1>
+                        </Tab>
+                        <Tab className="mr-2 py-2">
+                            <h1 className="px-3  border-b-2  border-transparent rounded-b-lg hover:text-red-300 hover:border-red-700 ">Anime</h1>
+                        </Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <h2>Any content 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Any content 2</h2>
+                    </TabPanel>
+                </Tabs>
+            </div>
+
+        </section>
     );
 };
 
--- src/Pages/Home/Toy_Gallary/Gallery.jsx
+++ src/Pages/Home/Toy_Gallary/Gallery.jsx
@@ -15,16 +15,16 @@ const Gallery = () => {
     }, [])
 
     return (
-        <div className="my-12">
+        <section className="my-12 mx-auto">
             <h1 className="text-5xl font-bold text-center my-8">Our Toy Gallery</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-8  mx-auto">
                 {
                     gallery.map(pic => <GalleryCard key={pic.id} gallery={pic}></GalleryCard>)
                 }
 
             </div>
 
-        </div>
+        </section>
     );
 };
 
