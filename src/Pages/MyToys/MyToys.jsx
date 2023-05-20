542fe2b9eccd9c3a92cf39c93a0c4f4ee7acdfd0
Mojaer Ahmed <mojaermohi@gmail.com>
2023-05-20 20:33:23 +0600

    functionality to delete toy is added

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import MyToysTable from "./MyToysTable";
+import Swal from "sweetalert2";
 
 
 const MyToys = () => {
@@ -18,15 +19,52 @@ const MyToys = () => {
         getToy()
     }, [user])
 
-    console.log(myToys)
+
+
+    //function to delete a toy
+    const deleteToy = (id) => {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/mytoys/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => {
+                        if (res.ok) {
+                            const resting = myToys.filter(toy => toy._id !== id)
+                            seMytToys(resting)
+                            Swal.fire(
+                                'Deleted!',
+                                'Your toy has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded p-10">
             <table className="w-full AllUserTable text-center text-gray-500">
-                <thead className="text-2xl text-gray-200 uppercase bg-transparent  ">
+                <thead className="text-xl text-gray-200 uppercase bg-transparent  ">
                     <tr >
                         <th scope="col" className="px-6 py-3">
+                            Image
+                        </th>
+                        <th scope="col" className="px-6 py-3">
                             Seller  Name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Seller  Email
                         </th>
                         <th scope="col" className="px-6 py-3">
                             Toy  Name
@@ -38,12 +76,18 @@ const MyToys = () => {
                             Price
                         </th>
                         <th scope="col" className="px-6 py-3">
+                            Rating
+                        </th>
+                        <th scope="col" className="px-6 py-3">
                             Available  quantity
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Description
                         </th>
                     </tr>
                 </thead>
                 {
-                    myToys.map(toy => <MyToysTable toy={toy} key={toy._id}></MyToysTable>)
+                    myToys.map(toy => <MyToysTable deleteToy={deleteToy} toy={toy} key={toy._id}></MyToysTable>)
                 }
             </table>
         </div>
--- src/Pages/MyToys/MyToysTable.jsx
+++ src/Pages/MyToys/MyToysTable.jsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
-const MyToysTable = ({ toy }) => {
+const MyToysTable = ({ toy, deleteToy }) => {
 
-    const { price, quantity, subCategory, toyName, sellerName, _id } = toy
+    const { price, quantity, subCategory, toyName, sellerName, _id,
+        description, imageUrl, rating, sellerEmail } = toy
+
+
+
+
     return (
         <tbody>
-            <tr className="bg-gray-700 text-gray-300  font-medium text-xl border-b ">
+            <tr className="bg-gray-700 text-gray-300  font-medium text-lg border-b ">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-200 whitespace-nowrap ">
+                    <img src={imageUrl} alt="" />
+                </th>
                 <th scope="row" className="px-6 py-4 font-medium text-gray-200 whitespace-nowrap ">
                     {sellerName}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-200 whitespace-nowrap ">
+                    {sellerEmail}
                 </th>
                 <td className="px-6 py-4">
                     {toyName}
@@ -20,10 +32,18 @@ const MyToysTable = ({ toy }) => {
                     ${price}
                 </td>
                 <td className="px-6 py-4">
+                    ${rating}
+                </td>
+                <td className="px-6 py-4">
                     {quantity}
                 </td>
+                <td className="px-6 py-4">
+                    {description.slice(0, 15) + '...'}
+                </td>
                 <th scope="col" className="px-6 py-3">
-                    <Link to={`/viewdetails/${_id}`} className="btn bg-gray-800 text-lg font-semibold">View Details</Link>
+                    <Link to={`/viewdetails/${_id}`} className="btn bg-gray-700 text-lg font-semibold">View</Link>
+                    <Link to={`/viewdetails/${_id}`} className="btn bg-gray-700 text-lg font-semibold">Edit</Link>
+                    <button onClick={() => deleteToy(_id)} className="btn bg-gray-700 text-lg text-red-500 font-semibold">delete</button>
                 </th>
             </tr>
         </tbody>
