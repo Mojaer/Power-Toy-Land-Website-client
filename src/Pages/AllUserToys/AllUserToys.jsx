571a8ab5088b1398fbc5f4f138c051921e2b77f8
Mojaer Ahmed <mojaermohi@gmail.com>
2023-05-21 20:32:09 +0600

    the sorting is implemented

--- src/Pages/AllUserToys/AllUserToys.jsx
+++ src/Pages/AllUserToys/AllUserToys.jsx
@@ -19,22 +19,52 @@ const AllUserToys = () => {
         const item = event.target.search.value
         const matchItem = allToys.filter(toy => toy.toyName.toLowerCase() === item.toLowerCase())
         setAllToys(matchItem)
-
     }
 
 
     const limitedToy = useMemo(() => allToys.slice(0, 20), [allToys]);
 
+
+    //sorting function for prices
+    const sortByLow = () => {
+        fetch('https://power-toy-land-server.vercel.app/sort/1')
+            .then((response) => response.json())
+            .then(data => setAllToys(data));
+    }
+
+    const sortByHigh = () => {
+        fetch('https://power-toy-land-server.vercel.app/sort/-1')
+            .then((response) => response.json())
+            .then(data => setAllToys(data));
+
+    }
+
     // console.log(limitedToy)
     return (
 
         <div className="relative overflow-x-auto shadow-md sm:rounded p-10">
-            <form onSubmit={searchItem}>
-                <div className="my-7 flex md:justify-end">
-                    <input type="text" name="search" className="bg-transparent border border-r-0 border-gray-300 text-white text-lg rounded-lg block pl-5 p-2.5 -mr-2 " required />
-                    <input type="submit" className="rounded-lg  pl-5 p-2.5 font-semibold border border-l-0 border-gray-300 bg-blue-700" value='Search' />
+
+            <div className="flex items-center justify-end">
+                <form onSubmit={searchItem}>
+                    <div className="my-7 flex md:justify-end mr-4">
+                        <input type="text" name="search" className="bg-transparent border border-r-0 border-gray-300 text-white text-lg rounded-lg block pl-5 p-2.5 -mr-2 " required />
+                        <input type="submit" className="rounded-lg  pl-5 p-2.5  font-semibold border border-l-0 border-gray-300 bg-blue-700" value='Search' />
+                    </div>
+                </form>
+
+                <div className="my-3 ">
+                    <div className="dropdown dropdown-bottom dropdown-end">
+                        <label tabIndex={0} className="btn m-1 bg-blue-700">Sort</label>
+                        <ul tabIndex={0} className="text-black dropdown-content menu p-2 shadow bg-base-300 rounded w-52">
+                            <li><button onClick={sortByLow} >By Lowest price</button></li>
+                            <li><button onClick={sortByHigh}>By Highest price</button></li>
+                        </ul>
+                    </div>
                 </div>
-            </form>
+            </div>
+
+
+
             <table className="w-full AllUserTable text-center text-gray-500" >
                 <thead className="text-2xl text-gray-200 uppercase bg-transparent  ">
                     <tr >
